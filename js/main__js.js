42494a5cb7b42f581b3fd759c4e9eb3c00b66e6f
Sergeq-mouse-code <moisecode99@gmail.com>
2020-02-07 20:36:20 +0300

    add

---
+++ js/main__js.js
@@ -0,0 +1,23 @@
+// let arr = [],
+//     set = new Set();
+// let number = 10000000;
+// for (let i = 0; i < number; i++) {
+//     arr.push(i);
+//     set.add(i);
+// };
+// console.time('Array');
+// let result = checkArr(arr, 555555);
+// console.log(result);
+// console.timeEnd('Array');
+// console.time('Set');
+// result = checkSet(set, 555555);
+// console.log(result);
+// console.timeEnd('Set');
+// let obj__set = new Set();
+// obj__set.add('Love');
+// obj__set.add('Fade');
+// obj__set.add('Black Velvet');
+// let arr = [...obj__set];
+// for (let item of arr) {
+//     alert(item);
+// }
